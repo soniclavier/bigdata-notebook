1276341b06d2de468d20d25d57140b6b56ca39a0
Vishnu Viswanath <vishnu.viswanath25@gmail.com>
2015-11-21 00:45:38 -0600

    Added underfit example for kaggle titanic problem

--- spark/src/main/scala/com/vishnu/spark/kaggle/TitanicUnderfit.scala
+++ spark/src/main/scala/com/vishnu/spark/kaggle/TitanicUnderfit.scala
@@ -19,55 +19,44 @@ import org.apache.spark.sql.DataFrame
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.Row
 
+object TitanicUnderfit {
 
-object TitanicOverfit {
-
-  def prepareOverfitData(data: DataFrame, train: Boolean,sqlContext: SQLContext): DataFrame = {
-    val toDouble = sqlContext.udf.register("toDouble",((n:Int) => {n.toDouble}))
-    var preparedData = data.drop("Name").drop("Ticket")
+  /**
+   * Keep age as the only feature.
+   */
+  def prepareUnderfitData(data: DataFrame, train: Boolean, sqlContext: SQLContext): DataFrame = {
+    val toDouble = sqlContext.udf.register("toDouble", ((n: Int) => { n.toDouble }))
+    var preparedData = data.drop("Pclass").drop("Name").drop("SibSp").drop("Parch").drop("Ticket").drop("Fare").drop("Cabin").drop("Embarked")
     if (train)
       preparedData = preparedData.drop("PassengerId")
     else
       preparedData = preparedData.withColumn("PassengerId", toDouble(preparedData("PassengerId")))
     var indexer = new StringIndexer().setInputCol("Sex").setOutputCol("SexIndex").fit(preparedData)
     preparedData = indexer.transform(preparedData)
-    indexer = new StringIndexer().setInputCol("Cabin").setOutputCol("CabinIndex").fit(preparedData)
-    preparedData = indexer.transform(preparedData)
-    indexer = new StringIndexer().setInputCol("Embarked").setOutputCol("EmbarkedIndex").fit(preparedData)
-    preparedData = indexer.transform(preparedData)
-    preparedData = preparedData.drop("Sex").drop("Cabin").drop("Embarked")
-
+    preparedData = preparedData.drop("Sex")
     if (train)
-      preparedData = preparedData.withColumn("Pclass", toDouble(preparedData("Pclass"))).withColumn("SibSp", toDouble(preparedData("SibSp"))).withColumn("Parch", toDouble(preparedData("Parch"))).withColumn("Survived", toDouble(preparedData("Survived")))
-    else
-      preparedData = preparedData.withColumn("Pclass", toDouble(preparedData("Pclass"))).withColumn("SibSp", toDouble(preparedData("SibSp"))).withColumn("Parch", toDouble(preparedData("Parch")))
+      preparedData = preparedData.withColumn("Survived", toDouble(preparedData("Survived")))
     return preparedData
   }
+
   def main(args: Array[String]) {
-    
-    val conf = new SparkConf().setAppName("Titanic Overfit").setMaster("spark://Vishnus-MacBook-Pro.local:7077")
+
+    val conf = new SparkConf().setAppName("Titanic Underfit").setMaster("spark://Vishnus-MacBook-Pro.local:7077")
     val sc = new SparkContext(conf)
     val sqlContext = new SQLContext(sc)
     var train_data = sqlContext.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").load("/kaggle/titanic/train.csv").toDF("PassengerId", "Survived", "Pclass", "Name", "Sex", "Age", "SibSp", "Parch", "Ticket", "Fare", "Cabin", "Embarked")
-    var prepared_train = prepareOverfitData(train_data, true,sqlContext)
+    var prepared_train = prepareUnderfitData(train_data, true, sqlContext)
     var trainLabeled = prepared_train.map { row: Row =>
       val features = Array[Double](row(1).asInstanceOf[Double],
-        row(2).asInstanceOf[Double],
-        row(3).asInstanceOf[Double],
-        row(4).asInstanceOf[Double],
-        row(5).asInstanceOf[Double],
-        row(6).asInstanceOf[Double],
-        row(7).asInstanceOf[Double],
-        row(8).asInstanceOf[Double])
+        row(2).asInstanceOf[Double])
       LabeledPoint(row(0).asInstanceOf[Double], Vectors.dense(features))
     }
 
     //val splits = trainLabeled.randomSplit(Array(0.6, 0.4), seed = 11L)
     //val training = splits(0).cache()
     //val test = splits(1)
-    
+
     val model = new LogisticRegressionWithLBFGS().setNumClasses(2).run(trainLabeled)
-    
 
     /*
     val predictionAndLabels = test.map {
@@ -78,31 +67,24 @@ object TitanicOverfit {
 
     val metrics = new MulticlassMetrics(predictionAndLabels)
     val precision = metrics.precision
-    println("Precision = " + precision) 
-    */
-
-    model.save(sc, "/kaggle/titanic/overfit_model")
+    println("Precision = " + precision)
+		*/
+    model.save(sc, "/kaggle/titanic/underfit_model")
 
     var submission_data = sqlContext.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").load("/kaggle/titanic/test.csv").toDF("PassengerId", "Pclass", "Name", "Sex", "Age", "SibSp", "Parch", "Ticket", "Fare", "Cabin", "Embarked")
-    var prepared_submission = prepareOverfitData(submission_data, false,sqlContext)
+    var prepared_submission = prepareUnderfitData(submission_data, false,sqlContext)
     var submissionLabeled = prepared_submission.map { row: Row =>
       val features = Array[Double](row(1).asInstanceOf[Double],
-        row(2).asInstanceOf[Double],
-        row(3).asInstanceOf[Double],
-        row(4).asInstanceOf[Double],
-        row(5).asInstanceOf[Double],
-        row(6).asInstanceOf[Double],
-        row(7).asInstanceOf[Double],
-        row(8).asInstanceOf[Double])
+        row(2).asInstanceOf[Double])
       LabeledPoint(row(0).asInstanceOf[Double], Vectors.dense(features))
     }
-    
+
     val submissionPrediction = submissionLabeled.map {
       case LabeledPoint(label, features) =>
         val prediction = model.predict(features)
         (label.toInt, prediction.toInt)
     }
-    
-    submissionPrediction.saveAsTextFile("/kaggle/titanic/overfit_output")
+       
+    submissionPrediction.saveAsTextFile("/kaggle/titanic/underfit_output")
   }
 }
