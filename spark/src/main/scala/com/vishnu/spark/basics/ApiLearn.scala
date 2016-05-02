b0baca38c9677fde3f67104c036a956e0cd4b602
Vishnu Viswanath <vishnu.viswanath25@gmail.com>
2016-05-01 21:26:51 -0500

    spark rdd map

--- spark/src/main/scala/com/vishnu/spark/basics/ApiLearn.scala
+++ spark/src/main/scala/com/vishnu/spark/basics/ApiLearn.scala
@@ -45,7 +45,7 @@ object ApiLearn {
     val b = sc.parallelize(List(1, 2, 3, 4, 5, 6, 7, 8, 9), 3)
     val part = b.partitions(0)
     val partRDD = b.mapPartitionsWithIndex((a, b) => if (a == part.index) b else Iterator(), true)
-    
+
     //filter
     intRDD.filter(x => x > 2)
     val address = sc.parallelize(List(("123", "Street1", "City1"), ("432", "Street2", "City2")))
@@ -56,26 +56,36 @@ object ApiLearn {
     //res47: Array[((String, String, String), Long)] = Array(((123,Street1,City1),0), ((432,Street2,City2),1))
 
     //foreachPartitions (returns nothing, can be used to perform some operation per partition, common example is DB connection
-    address.foreachPartition{partition=>
+    address.foreachPartition { partition =>
       val dummyDB = new DummyDB
       //or any other operation that has to be done once per partition
-      partition.foreach{case (a,b,c) => dummyDB.save(Address(a.toInt,b,c))}
+      partition.foreach { case (a, b, c) => dummyDB.save(Address(a.toInt, b, c)) }
     }
     //note this does not print anything to console, but if you check the application stdout from the spark ui(localhost:8080) you can see the following
     //in printed 
     //123 saved
     //432 saved
-    
+
     //for each item in address, do something. again, this does not print anything in the driver console instead prints in the stdout of worker
-    address.foreach(x=>println(x))
-    
-    
-   }
+    address.foreach(x => println(x))
+
+    //map vs foreach
+    //foreach and map iterates through the elements in the given RDD (collection)
+    //foreach does not return anything, instead it applies the given function on each element.
+    //map applies the given function and returns the new/transformed collection
+    address.map(x => (x._1, x))
+    address.map(transform).collect //here transform is a function that takes a Tuple3 give another Tuple2 (f: T => U)
+    //mapPartitions applies the given function to each partition. the function should be of the form, f: Iterator[T] => Iterator[U]
+  }
+
+  def transform(add:(String,String,String)) = {
+    (add._1, Address(add._1.toInt,add._2,add._3))
+  }
 
   case class Address(houseNumber: Int, street: String, city: String)
   class DummyDB {
-    def save(item:Address) {
-      println(item.houseNumber+" saved")
+    def save(item: Address) {
+      println(item.houseNumber + " saved")
     }
   }
 }
