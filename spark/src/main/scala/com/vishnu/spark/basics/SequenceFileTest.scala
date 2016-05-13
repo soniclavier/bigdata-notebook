e1457bb2ba2fbcfafe12fb5b83f6482a757b7796
Vishnu Viswanath <vishnu.viswanath25@gmail.com>
2016-05-13 16:14:23 -0500

    spark sequence file write

--- spark/src/main/scala/com/vishnu/spark/basics/SequenceFileTest.scala
+++ spark/src/main/scala/com/vishnu/spark/basics/SequenceFileTest.scala
@@ -2,6 +2,8 @@ package com.vishnu.spark.basics
 
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
+import org.apache.hadoop.io.Text
+import org.apache.hadoop.io.IntWritable
 
 object SequenceFileTest {
   
@@ -12,6 +14,11 @@ object SequenceFileTest {
     //create a sequence file
     val data = sc.parallelize(List(("key1",1), ("key2",2)))
     data.saveAsSequenceFile("/usr/vishnu/spark_temp/seqfile_sample")
+    
+    //read from sequence file
+    val dataLoaded = sc.sequenceFile("/usr/vishnu/spark_temp/seqfile_sample/part-00003", classOf[Text], classOf[IntWritable])
+    dataLoaded.foreach(println)
+    
   }
   
   
