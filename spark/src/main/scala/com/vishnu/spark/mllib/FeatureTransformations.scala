6408ff178bb678dfbb062a2196f53db20c0bc3c8
Vishnu Viswanath <vishnu.viswanath25@gmail.com>
2016-05-15 13:13:39 -0500

    spark mllib scaler example

---
+++ spark/src/main/scala/com/vishnu/spark/mllib/FeatureTransformations.scala
@@ -0,0 +1,25 @@
+package com.vishnu.spark.mllib
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.mllib.feature.StandardScaler
+import org.apache.spark.mllib.feature.Normalizer
+
+object FeatureTransformations {
+  
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("FeatureTransfomrations")
+    val sc = new SparkContext(conf)
+    
+    val vectors = List(Vectors.dense(Array(-2.0,5.0,1.0)),Vectors.dense(Array(2.0,0.0,1.0)))
+    val dataset = sc.parallelize(vectors)
+    
+    //with mean = true, with std = true
+    val scaler = new StandardScaler(true,true)    
+    val scalerModel = scaler.fit(dataset)
+    scalerModel.transform(dataset).collect.foreach(println)
+    
+   
+  }
+}
