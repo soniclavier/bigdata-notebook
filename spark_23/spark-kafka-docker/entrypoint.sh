6cbd05e3709270048b9433ad91c99ddc4f9b1589
Vishnu Viswanath <vishnu.viswanath25@gmail.com>
2018-02-27 09:25:35 -0500

    separate zookeeper to enable kafka scaling

--- spark_23/spark-kafka-docker/entrypoint.sh
+++ spark_23/spark-kafka-docker/entrypoint.sh
@@ -7,14 +7,14 @@ case $1 in
     spark-worker) exec $SPARK_HOME/bin/spark-class org.apache.spark.deploy.worker.Worker $2
             ;;
 
+    zookeeper) exec $KAFKA_HOME/bin/zookeeper-server-start.sh $KAFKA_HOME/config/zookeeper.properties
+            ;;
+
     kafka-broker)
-            echo "starting zookeeper"
-            setsid $KAFKA_HOME/bin/zookeeper-server-start.sh $KAFKA_HOME/config/zookeeper.properties >/dev/null 2>&1 < /dev/null &
-            sleep 2
-            echo "starting kafka-broker"
+            sed -r -i "s/(zookeeper.connect)=(.*)/\1=$2:2181/g" $KAFKA_HOME/config/server.properties
             exec $KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties
             ;;
 
-    *) echo "Unknown entrypoint $1, valid entry points are [spark-master, spark-worker <master-url>, kafka-broker]"
+    *) echo "Unknown entrypoint $1, valid entry points are [spark-master, spark-worker <master-url>, zookeeper, kafka-broker <zk-host>]"
             ;;
 esac
