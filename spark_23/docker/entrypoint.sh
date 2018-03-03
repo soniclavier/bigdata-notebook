3d5d824851ebdd3a75dfa300d27544488351d7b0
Vishnu Viswanath <vishnu.viswanath25@gmail.com>
2018-03-02 21:37:05 -0500

    remove old docker files

--- spark_23/docker/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-case $1 in
-    master) exec $SPARK_HOME/bin/spark-class org.apache.spark.deploy.master.Master
-            ;;
-
-    worker) exec $SPARK_HOME/bin/spark-class org.apache.spark.deploy.worker.Worker $2
-            ;;
-esac
